a2eb2e5f7ffb526210c502536302ed2993d68f90
Íñigo Aréjula Aísa <arejula27@gmail.com>
2019-07-16 19:21:39 +0200

    añadimos el icono en utilidades

--- lib/src/utils/icon_string_util.dart
+++ lib/src/utils/icon_string_util.dart
@@ -5,7 +5,10 @@ final Map<String, IconData> _icons={
 
   "add_alert":Icons.add_alert,
    "accessibility":Icons.accessibility,
-  "folder_open":Icons.folder_open
+  "folder_open":Icons.folder_open,
+  "donut_large":Icons.donut_large,
+
+
 
 
 } ;
