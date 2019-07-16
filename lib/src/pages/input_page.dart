d70d74382d68fe8c76b4c953ad184ec152e1327c
Íñigo Aréjula Aísa <arejula27@gmail.com>
2019-07-16 20:05:51 +0200

    Creacion inputPage
    Se ha creado la input Page y se han creado sus rutas y puesto su icono en los respectivos ficheros

---
+++ lib/src/pages/input_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class inputPage extends StatefulWidget {
+  @override
+  _inputPageState createState() => _inputPageState();
+}
+
+class _inputPageState extends State<inputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
