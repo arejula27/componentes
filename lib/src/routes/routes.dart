f4746cfe70ad46b4568b9dd520fc0ce0d36c1cae
Íñigo Aréjula Aísa <arejula27@gmail.com>
2019-07-16 19:19:44 +0200

    cambiado el archivo de rutas

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,6 +3,8 @@ import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
+
 
 
 
@@ -14,5 +16,6 @@ import 'package:componentes/src/pages/card_page.dart';
         'alert'     : (BuildContext context)=>AlertPage(),
         'avatar'    : (BuildContext context)=>AvatarPage(),
         'card'      : (BuildContext context)=>CardPage(),
+        'animatedContainer':(BuildContext context)=>AnimatedcontainerPage(),
         };
       }
